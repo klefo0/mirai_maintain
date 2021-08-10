f9490a999f1f29d7f2654c0fcb260d0aa64e04b8
Klefo0 <70630854+Klefo0@users.noreply.github.com>
2021-08-10 11:17:53 +0700

    Update help.js

--- modules/commands/help.js
+++ modules/commands/help.js
@@ -58,7 +58,7 @@ module.exports.run = function({ api, event, args, getText }) {
 			if (!group.some(item => item.group.toLowerCase() == commandConfig.config.commandCategory.toLowerCase())) group.push({ group: commandConfig.config.commandCategory.toLowerCase(), cmds: [commandConfig.config.name] });
 			else group.find(item => item.group.toLowerCase() == commandConfig.config.commandCategory.toLowerCase()).cmds.push(commandConfig.config.name);
 		}
-		group.forEach(commandGroup => msg += `「 ${commandGroup.group.charAt(0).toUpperCase() + commandGroup.group.slice(1)} 」\n${commandGroup.cmds.join(', ')}\n\n`);
+		group.forEach(commandGroup => msg += `(☞ﾟヮﾟ)☞ ${commandGroup.group.charAt(0).toUpperCase() + commandGroup.group.slice(1)} ☜(ﾟヮﾟ☜)\n${commandGroup.cmds.join(', ')}\n\n`);
 		return api.sendMessage(msg + getText("helpList", commands.size, prefix), threadID, async (error, info) =>{
 			if (autoUnsend) {
 				await new Promise(resolve => setTimeout(resolve, delayUnsend * 1000));
